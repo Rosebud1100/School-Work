4609010782ae2388257c99930b48203b3a7b3d9c
Rosebud1100 <154376220+Rosebud1100@users.noreply.github.com>
2026-09-18 09:46:36 -0500

    Add files via upload

---
+++ Cw2.py
@@ -0,0 +1,14 @@
+students = {"s1":{"name":"Ema", "major":"CS","year":"Freshman","gpa":"3.0"}}
+students2 =  {"s2" : {"name2": "your_name", "major2": "CS", "year":"freshman", "gpa2":"3.3"}}
+
+print("1. Add a student to a dictionary")
+print("2. Remove a student from a dictionary")
+print("3. Edit a student from a dictionary")
+print("4. Print a student from a dictionary")
+print ("5. Exit")
+
+while 1:
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        
+if choice == 1:
