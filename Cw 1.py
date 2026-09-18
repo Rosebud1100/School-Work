6a30ab2e1162426e77e314ae489402a69ae7da1c
Rosebud1100 <154376220+Rosebud1100@users.noreply.github.com>
2026-09-18 09:45:56 -0500

    Update Cw 1.py

--- Cw 1.py
+++ Cw 1.py
@@ -1,15 +1,29 @@
 #Menu driven program for add, del, replace
+myCourse = {}
+i=1
 while 1:
-    myCourse = {}
     print("1. Add Course")
     print("2. Remove Course")
     print("3. Replace Course")
     print("4. Print courses")
     print("5. Exit")
+
     choice = int(input("Enter your choice: "))
     if choice == 1:
-        course1 = input("Enter your course: ")
-        course2 = input("Enter your course: ")
-        course3 = input("Enter your course: ")
-        myCourse.update({"course1":course1, "course2":course2, "course3":course3})
+        course_name = input("Enter your course: ")
+        myCourse.update({"c"+str(i):course_name})
+        i=i+1
 
+    elif choice == 2:
+        del myCourse[input("Enter the course you want to delete: ")]
+
+    elif choice == 3:
+        myCourse.pop(input("Enter the course you want to delete: "))
+        course_replace = input("Enter your course: ")
+        myCourse.update({"c"+str(i):course_replace})
+
+    elif choice == 4:
+        print(myCourse)
+
+    else:
+        break
